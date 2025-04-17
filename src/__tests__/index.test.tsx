a8ffc388ab8ddc873a6d97557351774fc52a892e
Deromir Neves Jr <deromir@deromac.local>
2025-04-17 15:32:27 +0200

    Setup tests

---
+++ src/__tests__/index.test.tsx
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render } from "@testing-library/react";
+import Page from "../app/page"
+
+describe("HelloWorld", () => {
+  it("renders hello world", () => {
+    const { container } = render(<Page />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
