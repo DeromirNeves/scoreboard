f1d07703fd7bde54ee87230a3ab29f63954cf426
Deromir Neves Jr <deromir@deromac.local>
2025-04-17 19:18:37 +0200

    Add static score

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div><p>Hello World!</p></div>
+    <div>
+      <span>COL</span><span>1</span><span>x</span><span>2</span><span>FEC</span><span>71:12</span>
+    </div>
   );
 }
